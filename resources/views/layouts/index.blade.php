1cabe8ae23f54089a8675a760241c5741f68295d
prmdyazz <96212400+pramudyaazziz@users.noreply.github.com>
2023-09-03 22:07:53 +0700

    update layout

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -8,22 +8,32 @@
     <link rel="icon" type="image/x-icon" href="{{asset('favicon.png')}}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @stack('css')
+    <style>
+        .container {
+            display: grid;
+            grid-template-rows: auto 1fr auto;
+            min-height: 100vh;
+        }
+    </style>
 </head>
 <body>
-    <div class="container min-vh-100 justify-content-between d-flex flex-column">
+    <div class="container">
         <nav class="navbar">
             <a class="navbar-brand" href="{{route('home')}}">
                 <img src="{{asset('logo.png')}}" alt="Logo" width="200">
             </a>
         </nav>
-        <div class="flex-fill">
+        <div class="content">
             @yield('content')
         </div>
         <footer style="color: var(--secondary)" class="py-4 text-end">
             &copy; {{config('footer.year')}} - {{config('footer.domain')}}
         </footer>
     </div>
+
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous"></script>
+    @stack('js')
 </body>
 </html>
