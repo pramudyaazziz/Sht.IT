7620ab110c4ac87c016cad0ef2d52b5d5a521441
prmdyazz <96212400+pramudyaazziz@users.noreply.github.com>
2023-09-07 07:47:41 +0700

    update(controller): remove useless namespace on HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
